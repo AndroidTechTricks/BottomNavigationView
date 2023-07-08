749711cb805bec836442e6cb220c8eb38ba049ae
parasjha <parasjhapr@gmail.com>
2023-07-08 15:46:47 +0530

    add dark mode and nav

--- app/src/main/java/com/example/androidtechtricks/MainActivity.kt
+++ app/src/main/java/com/example/androidtechtricks/MainActivity.kt
@@ -2,15 +2,34 @@ package com.example.androidtechtricks
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.androidtechtricks.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        navController = findNavController(R.id.nav_host_fragment_activity_main)
+
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home,
+                R.id.navigation_fav,
+                R.id.navigation_search,
+                R.id.navigation_profile
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        binding.bottomnavigation.setupWithNavController(navController)
     }
 }
